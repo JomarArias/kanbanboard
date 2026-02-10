73cf13cc95a927d48b75089c00d395ceb377a735
JomarArias <practicas11@dappertechnologies.com>
2026-02-10 15:21:37 -0500

    feat: mejora de diseño
    se mejoro el diseño de todo el frontend optimizando y agregando mas componentes de funcionalidades

--- frontend/src/app/features/kanban/components/kanban-card/kanban-card.component.ts
+++ frontend/src/app/features/kanban/components/kanban-card/kanban-card.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Kanban } from '../../../../core/models/kanban.model';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 
 @Component({
   selector: 'app-kanban-card',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, ToastModule, ConfirmPopupModule],
   templateUrl: './kanban-card.component.html',
   styleUrl: './kanban-card.component.scss',
   host: {
@@ -16,11 +20,23 @@ export class KanbanCardComponent {
   @Output() edit = new EventEmitter<Kanban>();
   @Output() delete = new EventEmitter<string>();
 
+  constructor(private confirmationService: ConfirmationService, private messageService: MessageService) { }
+
   onEdit() {
     this.edit.emit(this.card);
   }
 
-  onDelete() {
-    this.delete.emit(this.card.id);
+  onDelete(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: '¿Estás seguro de eliminar esta tarjeta?',
+      header: 'Confirmación',
+      acceptLabel: 'Sí',
+      rejectLabel: 'No',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.delete.emit(this.card.id);
+      }
+    });
   }
 }
