08efe0c2c1aa6cb5c321c5d07613b46f95da107a
Jusit-Toledo <jusittoledob51@gmail.com>
2026-03-09 14:21:52 -0500

    feat(upload): permitir carga de foto de perfil desde dispositivo

--- backend/src/controllers/upload.controller.ts
+++ backend/src/controllers/upload.controller.ts
@@ -1,3 +1,4 @@
+// import { uploadCardImage } from './upload.controller';
 import { Request, Response } from "express";
 import { sendError } from "../utils/http-response.js";
 import { uploadImageBuffer } from "../services/upload.service.js";
@@ -34,3 +35,27 @@ export const uploadCardImage = async (req: Request, res: Response) => {
     return sendError(res, 500, err?.message || "Error subiendo imagen");
   }
 };
+
+export const uploadProfileImage = async (req: Request, res: Response) => {
+  try {
+    const file = req.file
+
+    if (!file) return sendError (res, 400, "Archivo requiere un campo file")
+
+    if (!ALLOWED_IMAGE_MIME_TYPES.has(file.mimetype)){
+      return sendError (
+        res, 400, "Formato no permitido.Usa JPG, PNG o WEBP"
+      )
+    }
+    const uploaded = await uploadImageBuffer(file.buffer, "kanban/profiles")
+
+    return res.status(201).json({
+      imageUrl: uploaded.imageUrl,
+      publicId: uploaded.publicId
+    })
+  } catch (err: any) {
+    return sendError(res, 500, err?.message || "Error subiendo imagen de perfil");
+    
+  }
+};
+
